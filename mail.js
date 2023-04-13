ce3bb36604fd962bf9ae7e4dba170e31e0a5ef87
UNknown-monKEY <89349387+UNknown-monKEY@users.noreply.github.com>
2023-04-13 09:33:38 +0530

    Update mail.js

--- mail.js
+++ mail.js
@@ -1,15 +1,4 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDLkeddziTg3KSs4xN0sRSSzEfn0WBNJh0",
-    authDomain: "food-fba3f.firebaseapp.com",
-    databaseURL: "https://food-fba3f-default-rtdb.firebaseio.com",
-    projectId: "food-fba3f",
-    storageBucket: "food-fba3f.appspot.com",
-    messagingSenderId: "544146435715",
-    appId: "1:544146435715:web:58c059a39d85670cbe35b6"
-  };
-  
- firebase.initializeApp(firebaseConfig);
- document.getElementById("donate-form").addEventListener("submit", submitForm);
+var database =firebase.database();
 
 function submitForm(e) {
   e.preventDefault();
@@ -19,8 +8,16 @@ function submitForm(e) {
   var phone = getElementVal("phone");
   var address = getElementVal("address");
   var description = getElementVal("description");
+    
+    database.ref('user/').push().set({
+        name:name,
+        emailid:emailid,
+        phone:phone,
+        address:address,
+        description=description,
+    }
 
-  saveMessages(name, emailid, phone, address, description);
+
 
   //   enable alert
   document.querySelector(".alert").style.display = "block";
